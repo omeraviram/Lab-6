884e487bf6f00d518ea9bcc31ca12bb288d7b47b
omeraviram <omeraviram@ufl.edu>
2023-10-25 11:48:07 -0400

    encode function
    encode function

---
+++ main.py
@@ -0,0 +1,30 @@
+#Omer Aviram
+#partner name is Jacob
+
+def encode_pass(password):
+    result = ""
+    for char in password:
+        if char.isdigit():
+            new_digit = str(int(char) + 3)
+            result += new_digit
+        else:
+            result += char
+    return result
+
+
+
+
+while True:
+
+    print("Menu")
+    print("------------")
+    print("1. Encode\n2. Decode\n3. Quit\n")
+    choice = (int(input("Please enter an option: ")))
+
+    if choice == 1:
+        password = (input("Please enter your password to encode: "))
+        new_password = encode_pass(password)
+        print("Your password has been encoded and stored!\n")
+
+    if choice == 3:
+        break
